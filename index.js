e8d11f6b72e12fdda4a700b559aeaa1326b94f42
Arpit <arpitbhushan1@gmail.com>
2021-05-26 00:01:32 +0530

    views

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = 8000;
 app.use('/',require('./routes'))
 
 //set up the view engine
-app.set('view engine',ejs);
-app.set('views','/views');
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
 app.listen(port,function(err){
