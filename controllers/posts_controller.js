ac4a6ca9a994d14e1d2febb6d723b0851561cabf
Arpit <arpitbhushan1@gmail.com>
2021-05-25 17:32:47 +0530

    Added a few more routers and controllers

---
+++ controllers/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.photos = (function(req,res){
+    return res.end("photos");
+})
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,10 @@ const homeControleer = require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/',homeControleer.home);
+router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+
+//for any further routes, acess from here
+//router.use('/routername', require('./routerfile'));
 
 module.exports = router;
